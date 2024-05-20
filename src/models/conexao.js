1165d7455323f0be3e780214e2f322b44524e83f
Tteu <129115951+TteuLanchess@users.noreply.github.com>
2024-05-20 08:56:11 -0300

    conexão ao banco de dados funcionando

--- src/models/conexao.js
+++ src/models/conexao.js
@@ -4,8 +4,8 @@ const conexao = mysql.createConnection({
     host: 'localhost',
     port: '3306',
     user: 'root',
-    password: '1234567',
-    database: 'musify'
+    password: 'password',
+    database: 'musicfy'
 })
 
 conexao.connect(erro => {
